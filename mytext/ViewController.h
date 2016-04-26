14dc11c664a70f2507bfba4a25d8563fd7f139d3
xusijun <xusijun@xusijundemacbook-air.local>
2016-04-26 10:40:36 +0800

    Initial Commit

---
+++ mytext/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mytext
+//
+//  Created by xusijun on 16/4/26.
+//  Copyright © 2016年 xusijun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mytextTests/mytextTests.m
@@ -0,0 +1,39 @@
+//
+//  mytextTests.m
+//  mytextTests
+//
+//  Created by xusijun on 16/4/26.
+//  Copyright © 2016年 xusijun. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mytextTests : XCTestCase
+
+@end
+
+@implementation mytextTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
